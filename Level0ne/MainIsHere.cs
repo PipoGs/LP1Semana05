88ef7e70a7cd20a6aa7ec6687307c5543445a0ee
Filipe Goncalves <filipegoncalves.dev@gmail.com>
2025-03-20 22:53:09 +0000

    Add method doc

--- Level0ne/MainIsHere.cs
+++ Level0ne/MainIsHere.cs
@@ -2,8 +2,14 @@
 
 namespace Level0ne
 {
+
     public class Program
     {
+        /// <summary>
+        /// Main entry point for the application.
+        /// This is the first method that is called when the program is run.
+        /// </summary>
+        /// <param name="args"></param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
