016d89043ca6a7403b5d589bb23d18bd401c5146
Filipe Goncalves <filipegoncalves.dev@gmail.com>
2025-03-21 17:52:19 +0000

    Tried adding arguments

--- CarStats/Program.cs
+++ CarStats/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Spectre.Console;
 using Bogus;
+using Microsoft.VisualBasic;
 
 namespace CarStats
 {
@@ -9,19 +10,27 @@ namespace CarStats
 
         private static void Main(string[] args)
         {
+            Faker faker = new Faker();
 
-            string cars = new Faker().Vehicle.Manufacturer();
-            double numbers = new Faker().Random.Number(1, 20);
+            string input = args[0];
+            int Count = int.Parse(input);
 
 
+            BarChart bc = new BarChart()
+                .Width(60)
+                .Label("[white bold underline]Car Sales[/]")
+                .CenterLabel();
 
-            AnsiConsole.Write(new BarChart()
-            .Width(60)
-            .Label("Car Sales[/]")
-            .CenterLabel()
-            .AddItem(cars, numbers, Color.DarkSlateGray1)
-            .AddItem(cars, numbers, Color.DarkSlateGray1)
-            .AddItem(cars, numbers, Color.DarkSlateGray1));
+            for (int i = 0; i < Count; i++)
+            {
+                string Brand = faker.Vehicle.Manufacturer();
+                int Number = faker.Random.Number(1, 20);
+                Color color = new Color(faker.Random.Byte(), faker.Random.Byte(), faker.Random.Byte());
+
+                bc.AddItem(Brand, Number, color);
+            }
+
+            AnsiConsole.Write(bc);
         }
     }
 }
