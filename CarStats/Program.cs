b9d2db2d0480467f32d1147cfaaab251aacadf9d
Filipe Goncalves <filipegoncalves.dev@gmail.com>
2025-03-21 17:20:41 +0000

    Added first Tries

---
+++ CarStats/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Spectre.Console;
+using Bogus;
+
+namespace CarStats
+{
+    public class Program
+    {
+
+        private static void Main(string[] args)
+        {
+
+            string cars = new Faker().Vehicle.Manufacturer();
+            double numbers = new Faker().Random.Number(1, 20);
+
+
+
+            AnsiConsole.Write(new BarChart()
+            .Width(60)
+            .Label("Car Sales[/]")
+            .CenterLabel()
+            .AddItem(cars, numbers, Color.DarkSlateGray1)
+            .AddItem(cars, numbers, Color.DarkSlateGray1)
+            .AddItem(cars, numbers, Color.DarkSlateGray1));
+        }
+    }
+}
