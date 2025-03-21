3d8ca62d3bc7b4f5dc213b77981a77bbee0ad2af
Filipe Goncalves <filipegoncalves.dev@gmail.com>
2025-03-21 00:39:51 +0000

    Added LevelTwo and code

--- LevelTwo/Program.cs
+++ LevelTwo/Program.cs
@@ -5,11 +5,24 @@ namespace LevelTwo
 {
     public class Program
     {
-        private static void Main()
+        private static void Main(string[] args)
         {
-            CanvasImage image = new CanvasImage("tux.jpg");
+            string input = args[0];
+            int maxWidth = 24;
 
-            image.MaxWidth(20);
+            if (args.Length > 0)
+            {
+                input = args[0];
+            }
+
+            if (args.Length > 1 && int.TryParse(args[1], out int width))
+            {
+                maxWidth = width;
+            }
+
+            CanvasImage image = new CanvasImage(input);
+
+            image.MaxWidth(maxWidth);
 
             AnsiConsole.Write(image);
         }
